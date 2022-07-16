f850316415918cec2b98f55e680b13123bf5c7eb
t <hon.y23@student.passerellesnumeriques.org>
2022-07-16 16:53:00 +0700

    chinhsuacontent

--- src/pages/Conten/Content.js
+++ src/pages/Conten/Content.js
@@ -1,145 +1,65 @@
 import "./Content.css";
 import React, { Component } from "react";
-import Categories from "../../Mockdata/Categories.json"
+import Categories from "../../Mockdata/Categories.json";
+import showHomepage from "../../Mockdata/showHomepage.json";
 export default class Content extends Component {
   render() {
     return (
       <div className="Content">
         <div className="viewallcat">
           <a className="titel">Sửa Điện Thoại</a>
-          <br/>
-          {Categories && Categories.map((item, index) => <><a key={index}>{item}</a><br /></>)}
+          <br />
+          {Categories &&
+            Categories.map((item, index) => (
+              <>
+                <a key={index}>{item}</a>
+                <br />
+              </>
+            ))}
         </div>
+
         <div className="container">
-          <div className="prd">
-            <img src="https://huydungmobile.com/wp-content/uploads/2021/10/do-vo-iphone-xs-max-len-iphone-13-pro-max-201x201.jpg" />
-            <div className="title-wrapper">
-              <p>Độ vỏ iPhone Xs Max lên iPhone 13 Pro Max</p>
-            </div>
-            <div className="price-wrapper">
-              <p>2.300.000đ</p>
-            </div>
-          </div>
-          <div className="prd">
-            <img src="https://huydungmobile.com/wp-content/uploads/2021/12/do-vo-iphone-xs-max-len-iphone-12-pro-max-201x201.jpg" />
-            <div className="title-wrapper">
-              <p>Độ vỏ iPhone Xs Max lên iPhone 13 Pro Max</p>
-            </div>
-            <div className="price-wrapper">
-              <p>2.300.000đ</p>
-            </div>
-          </div>
-          <div className="prd">
-            <img src="https://huydungmobile.com/wp-content/uploads/2021/12/do-vo-iphone-xs-max-len-iphone-12-pro-max-201x201.jpg" />
-            <div className="title-wrapper">
-              <p>Độ vỏ iPhone Xs Max lên iPhone 13 Pro Max</p>
-            </div>
-            <div className="price-wrapper">
-              <p>2.300.000đ</p>
-            </div>
-          </div>
-          <div className="prd">
-            <img src="https://huydungmobile.com/wp-content/uploads/2021/05/thay-mat-kinh-iphone-x-mat-sau-201x201.jpg" />
-            <div className="title-wrapper">
-              <p>Thay mặt kính IphoneX</p>
-            </div>
-            <div className="price-wrapper">
-              <p>2.300.000đ</p>
-            </div>
-          </div>
-          <div className="prd">
-            <img src="https://huydungmobile.com/wp-content/uploads/2021/04/thay-mat-kinh-samsung-galaxy-a71-201x201.jpg" />
-            <div className="title-wrapper">
-              <p>Độ vỏ iPhone Xs Max lên iPhone 13 Pro Max</p>
-            </div>
-            <div className="price-wrapper">
-              <p>2.300.000đ</p>
-            </div>
-          </div>
-          <div className="prd">
-            <img src="https://huydungmobile.com/wp-content/uploads/2021/05/thay-mat-kinh-iphone-7-plus-201x201.jpg" />
-            <div className="title-wrapper">
-              <p>Độ vỏ iPhone Xs Max lên iPhone 13 Pro Max</p>
-            </div>
-            <div className="price-wrapper">
-              <p>2.300.000đ</p>
-            </div>
-          </div>
+          {showHomepage &&
+            showHomepage.map((item, index) => (
+              <>
+                <div className="prd">
+                  <img src={item.image} key={index} />
+                  <div className="title-wrapper">
+                    <p key={index}>{item.name}</p>
+                  </div>
+                  <div className="price-wrapper">
+                    <p key={index}>{item.price}</p>
+                  </div>
+                </div>
+              </>
+            ))}
         </div>
         <div className="viewallcat-tabled">
           <a className="titel">Sửa Máy Tính Bảng</a>
           <br />
-          <a>Thay ép mặt kính</a>
-          <br />
-          <a>Thay cảm ứng</a>
-          <br />
-          <a>Thay màn hình</a>
-          <br />
-          <a>Thay kính lưng</a>
-          <br />
-          <a>Thay pin</a>
-          <br />
-          <a>Thay camera</a>
-          <br />
-          <a>Thay chân sạc</a>
-          <br />
-          <a>Xem tất cả;</a>
+          {Categories &&
+            Categories.map((item, index) => (
+              <>
+                <a key={index}>{item}</a>
+                <br />
+              </>
+            ))}
         </div>
         <div className="container">
-          <div className="prd">
-            <img src="https://huydungmobile.com/wp-content/uploads/2021/05/thay-chan-sac-ipad-pro-11-inch-2018-201x201.jpg" />
-            <div className="title-wrapper">
-              <p>Độ vỏ iPhone Xs Max lên iPhone 13 Pro Max</p>
-            </div>
-            <div className="price-wrapper">
-              <p>2.300.000đ</p>
-            </div>
-          </div>
-          <div className="prd">
-            <img src="https://huydungmobile.com/wp-content/uploads/2021/05/thay-mat-kinh-ipad-air-4-201x201.jpg" />
-            <div className="title-wrapper">
-              <p>Độ vỏ iPhone Xs Max lên iPhone 13 Pro Max</p>
-            </div>
-            <div className="price-wrapper">
-              <p>2.300.000đ</p>
-            </div>
-          </div>
-          <div className="prd">
-            <img src="https://huydungmobile.com/wp-content/uploads/2021/05/thay-vo-ipad-pro-11-inch-2018-1-201x201.jpg" />
-            <div className="title-wrapper">
-              <p>Độ vỏ iPhone Xs Max lên iPhone 13 Pro Max</p>
-            </div>
-            <div className="price-wrapper">
-              <p>2.300.000đ</p>
-            </div>
-          </div>
-          <div className="prd">
-            <img src="https://huydungmobile.com/wp-content/uploads/2020/11/thay-mat-kinh-ipad-gen-8-2020-201x201.jpg" />
-            <div className="title-wrapper">
-              <p>Độ vỏ iPhone Xs Max lên iPhone 13 Pro Max</p>
-            </div>
-            <div className="price-wrapper">
-              <p>2.300.000đ</p>
-            </div>
-          </div>
-          <div className="prd">
-            <img src="https://huydungmobile.com/wp-content/uploads/2021/05/thay-pin-ipad-air-1-chinh-hang-pisen-201x201.jpg" />
-            <div className="title-wrapper">
-              <p>Độ vỏ iPhone Xs Max lên iPhone 13 Pro Max</p>
-            </div>
-            <div className="price-wrapper">
-              <p>2.300.000đ</p>
-            </div>
-          </div>
-          <div className="prd">
-            <img src="https://huydungmobile.com/wp-content/uploads/2020/11/thay-mat-kinh-ipad-gen-8-2020-201x201.jpg" />
-            <div className="title-wrapper">
-              <p>Độ vỏ iPhone Xs Max lên iPhone 13 Pro Max</p>
-            </div>
-            <div className="price-wrapper">
-              <p>2.300.000đ</p>
-            </div>
-          </div>
+          {showHomepage &&
+            showHomepage.map((item, index) => (
+              <>
+                <div className="prd">
+                  <img src={item.image} key={index} />
+                  <div className="title-wrapper">
+                    <p key={index}>{item.name}</p>
+                  </div>
+                  <div className="price-wrapper">
+                    <p key={index}>{item.price}</p>
+                  </div>
+                </div>
+              </>
+            ))}
         </div>
       </div>
     );
